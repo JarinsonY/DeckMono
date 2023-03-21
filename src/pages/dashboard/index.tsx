7a59e49a32b0c3eb0bcce4cd24459df87348e1a1
Jarinson Palacios <jyadith@gmail.com>
2023-03-20 20:23:12 -0500

    refactor page tree

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,3 @@
-import tw from 'twin.macro'
-import styled from 'styled-components';
-
 import Card from '@/components/Card';
 import Pagination from '@/components/Pagination';
 import Navbar from '@/components/Navbar';
@@ -8,27 +5,7 @@ import { POKEMONS_PER_PAGE } from '@/utils/constants';
 import useGetPokemonList from '@/hooks/useGetPokemonList';
 import { Pokemon, PokemonListResponse } from '@/utils/types';
 import Loader from '@/components/Loader';
-
-/* interface Pokemon {
-    id: number;
-    name: string;
-    image: string;
-    type: string;
-    weight: number;
-    moves: string[];
-} */
-
-const Main = styled.main`
-  ${tw`h-full flex flex-col pt-14 px-8 items-center justify-center`}
-`;
-
-const Container = styled.div`
-    ${tw`flex flex-col items-center justify-center flex-1 px-20 text-center`}
-`;
-
-const ContainerCards = styled.div`
-    ${tw`grid xl:grid-cols-5 sm:grid-cols-2 lg:grid-cols-2 gap-x-12 gap-y-8`}
-`;
+import { Container, ContainerCards, Main } from './styled';
 
 const Dashboard = () => {
     const { pokemonList, getPokemonList, isLoading } = useGetPokemonList();
